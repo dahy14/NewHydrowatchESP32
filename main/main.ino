4633841a8d5a5a25ca4b93066308c0b97845c212
John Louis Naypes <naypes.15@gmail.com>
2024-09-17 19:25:11 +0800

    totoo na to na 2 cores code

--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,9 @@
 #include "globalVars.h"
 #include "volume.h"
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 void setup()
 {
     initLCD();// From LCDOperations.h
@@ -19,14 +22,48 @@ void setup()
     initNTP(); // From NTPtime.h
     initFirebase(); // From FirebaseSetup.h
     clearLCD(); // From LCDOperations.h
+
+    Serial.println(xPortGetFreeHeapSize());
+    xTaskCreatePinnedToCore(
+                    core0Task,   // Task function. /
+                    "firebase operations",     / name of task. /
+                    40000,       // Stack size of task /
+                    NULL,        // parameter of the task /
+                    1,           // priority of the task /
+                    &Task1,      // Task handle to keep track of created task /
+                    0);          // pin task to core 0 /    
+    xTaskCreatePinnedToCore(
+                    core1Task,   // Task function. /
+                    "offline operations",     // name of task. /
+                    10000,       // Stack size of task /
+                    NULL,        // parameter of the task /
+                    2,           // priority of the task /
+                    &Task2,      // Task handle to keep track of created task /
+                    1);          // pin task to core 1 
 }
 
-void loop()
-{
-    displayVolume(); // From LCDOperations.h
+void core0Task( void * pvParameters ){
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+  for(;;){
     handleFirebase(); // From FirebaseOperations.h
     getSolenoid(); // From FirebaseOperations.h
-    setVolume(); // From FirebaseOperations.h
+    //setVolume(); // From FirebaseOperations.h
+
+  }
+}
+
+void core1Task( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+    displayVolume(); // From LCDOperations.h
+     yield();
+  }
+}
+void loop()
+{  
 }
 
 // cannot be put on module. let this stay here.... 
@@ -34,5 +71,3 @@ ICACHE_RAM_ATTR void increase()
 {
     pulse++;
 }
-
-
